97fe38b7aaa4fb0f9656c18d489783c8e059fa5c
Christoph Bühler <christoph@smartive.ch>
2020-12-28 11:40:58 +0100

    test: add additional testing

--- tests/DotnetKubernetesClient.Test/KubernetesClient.Test.cs
+++ tests/DotnetKubernetesClient.Test/KubernetesClient.Test.cs
@@ -1,36 +1,129 @@
 ﻿using System;
-using System.Threading;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
-using k8s;
-using Moq;
+using k8s.Models;
 using Xunit;
 
 namespace DotnetKubernetesClient.Test
 {
-    public class KubernetesClientTest
+    public class KubernetesClientTest : IDisposable
     {
         private readonly IKubernetesClient _client;
-        private readonly Mock<IKubernetes> _mock = new();
+        private readonly IList<V1ConfigMap> _objects = new List<V1ConfigMap>();
 
         public KubernetesClientTest()
         {
-            _client = new KubernetesClient(_mock.Object, KubernetesClientConfiguration.BuildDefaultConfig());
+            _client = new KubernetesClient();
         }
 
         [Fact]
-        public async  Task Should_Return_Namespace()
+        public async Task Should_Return_Namespace()
         {
             var ns = await _client.GetCurrentNamespace();
             ns.Should().Be("default");
         }
 
         [Fact]
-        public async  Task Should_Call_GetCode()
+        public async Task Should_Create_Some_Object()
         {
-            _mock.Setup(o => o.GetCodeWithHttpMessagesAsync(null, default));
-            await _client.GetServerVersion();
-            _mock.Verify(o => o.GetCodeWithHttpMessagesAsync(null, default), Times.Once);
+            var config = await _client.Create(
+                new V1ConfigMap
+                {
+                    Kind = V1ConfigMap.KubeKind,
+                    ApiVersion = V1ConfigMap.KubeApiVersion,
+                    Metadata = new(name: RandomName(), namespaceProperty: "default"),
+                    Data = new Dictionary<string, string> { { "Hello", "World" } },
+                });
+            _objects.Add(config);
+
+            config.Metadata.Should().NotBeNull();
+            config.Metadata.ResourceVersion.Should().NotBeNullOrWhiteSpace();
         }
+
+        [Fact]
+        public async Task Should_Update_Some_Object()
+        {
+            var config = await _client.Create(
+                new V1ConfigMap
+                {
+                    Kind = V1ConfigMap.KubeKind,
+                    ApiVersion = V1ConfigMap.KubeApiVersion,
+                    Metadata = new(name: RandomName(), namespaceProperty: "default"),
+                    Data = new Dictionary<string, string> { { "Hello", "World" } },
+                });
+            var r1 = config.Metadata.ResourceVersion;
+            _objects.Add(config);
+
+            config.Data.Add("test", "value");
+            await _client.Update(config);
+            var r2 = config.Metadata.ResourceVersion;
+
+            r1.Should().NotBe(r2);
+        }
+
+        [Fact]
+        public async Task Should_List_Some_Objects()
+        {
+            var config1 = await _client.Create(
+                new V1ConfigMap
+                {
+                    Kind = V1ConfigMap.KubeKind,
+                    ApiVersion = V1ConfigMap.KubeApiVersion,
+                    Metadata = new(name: RandomName(), namespaceProperty: "default"),
+                    Data = new Dictionary<string, string> { { "Hello", "World" } },
+                });
+            var config2 = await _client.Create(
+                new V1ConfigMap
+                {
+                    Kind = V1ConfigMap.KubeKind,
+                    ApiVersion = V1ConfigMap.KubeApiVersion,
+                    Metadata = new(name: RandomName(), namespaceProperty: "default"),
+                    Data = new Dictionary<string, string> { { "Hello", "World" } },
+                });
+
+            _objects.Add(config1);
+            _objects.Add(config2);
+
+            var configs = await _client.List<V1ConfigMap>("default");
+            configs.Count.Should().Be(2);
+        }
+
+        [Fact]
+        public async Task Should_Delete_Some_Object()
+        {
+            var config1 = await _client.Create(
+                new V1ConfigMap
+                {
+                    Kind = V1ConfigMap.KubeKind,
+                    ApiVersion = V1ConfigMap.KubeApiVersion,
+                    Metadata = new(name: RandomName(), namespaceProperty: "default"),
+                    Data = new Dictionary<string, string> { { "Hello", "World" } },
+                });
+            var config2 = await _client.Create(
+                new V1ConfigMap
+                {
+                    Kind = V1ConfigMap.KubeKind,
+                    ApiVersion = V1ConfigMap.KubeApiVersion,
+                    Metadata = new(name: "demo-config-two", namespaceProperty: "default"),
+                    Data = new Dictionary<string, string> { { "Hello", "World" } },
+                });
+            _objects.Add(config1);
+
+            var configs = await _client.List<V1ConfigMap>("default");
+            configs.Count.Should().Be(2);
+
+            await _client.Delete(config2);
+
+            configs = await _client.List<V1ConfigMap>("default");
+            configs.Count.Should().Be(1);
+        }
+
+        public void Dispose()
+        {
+            _client.Delete(_objects).Wait();
+        }
+
+        private static string RandomName() => "cm-" + Guid.NewGuid().ToString().ToLower();
     }
 }
---
+++ tests/DotnetKubernetesClient.Test/KubernetesClientWithMock.Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using k8s;
+using Moq;
+using Xunit;
+
+namespace DotnetKubernetesClient.Test
+{
+    public class KubernetesClientWithMockTest
+    {
+        private readonly IKubernetesClient _client;
+        private readonly Mock<IKubernetes> _mock = new();
+
+        public KubernetesClientWithMockTest()
+        {
+            _client = new KubernetesClient(_mock.Object, KubernetesClientConfiguration.BuildDefaultConfig());
+        }
+
+        [Fact]
+        public async  Task Should_Return_Namespace()
+        {
+            var ns = await _client.GetCurrentNamespace();
+            ns.Should().Be("default");
+        }
+    }
+}
