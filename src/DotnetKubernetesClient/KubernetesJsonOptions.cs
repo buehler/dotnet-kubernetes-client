9ec1653e7776dad755fefca9e5aee23755f03bf2
Mark Lopez <m@silvenga.com>
2022-05-23 16:52:58 -0500

    Fixed not using the K8s options in every case.

---
+++ src/DotnetKubernetesClient/KubernetesJsonOptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Text.Json;
+using k8s;
+
+#nullable enable
+
+namespace DotnetKubernetesClient
+{
+    internal static class KubernetesJsonOptions
+    {
+        private static readonly Lazy<JsonSerializerOptions?> Lazy = new(GetJsonSerializerOptions);
+
+        public static JsonSerializerOptions? DefaultOptions => Lazy.Value;
+
+        private static JsonSerializerOptions? GetJsonSerializerOptions()
+        {
+            try
+            {
+                // Get the default (and private) KubernetesJson options.
+                var fieldInfo = typeof(KubernetesJson).GetField("JsonSerializerOptions", BindingFlags.Static | BindingFlags.NonPublic);
+                if (fieldInfo != null
+                    && fieldInfo.GetValue(null) is JsonSerializerOptions options)
+                {
+                    return options;
+                }
+            }
+            catch
+            {
+                // Ignored
+            }
+
+            return null;
+        }
+    }
+}
